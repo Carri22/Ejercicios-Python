9836014facad1211ec3e7f458bff47c366347eb9
Carri22 <lucascarrizo11@gmail.com>
2023-02-09 11:55:41 -0300

    Consigna del ejercicio 8

--- Ejercito 8/mi_respuesta_8.py
+++ Ejercito 8/mi_respuesta_8.py
@@ -1,11 +1,20 @@
 
-#INCOMPLETO!!
+'''
+Complete la solución para que la función rompa la tripa de camello, usando un espacio entre palabras.
+
+Ejemplo
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+'''
+    
 
 import re
 
 def solution(s):
+    
     x = re.search("[A-Z]+", s)
-    text = re.sub("[A-Z]+", " "+ x.group(), s)
-    return text
+    s = re.sub("[A-Z]+", " "+ x.group(), s)
+    return s
 
 print(solution("holaComoEstas"))
